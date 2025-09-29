9fcf62eebf0d2fc31bf26daa807046c1624c48c8
Ravi Thakur <ravihakur@gmail.com>
2025-09-29 10:44:52 +0500

    Initial commit - Todo Web App

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "todo-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/to-do.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache or network
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
